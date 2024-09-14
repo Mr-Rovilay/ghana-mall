b6033c2c103918e7ed2a1b1355821f623a2d9864
ayodeji akinola <ayodejiakinola007@gmail.com>
2024-09-14 15:43:43 +0100

    navbar

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./components/Header"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Home from "./pages/Home"
+
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+     <Header/>
+     <Routes>
+      <Route path="" element={<Home/>}/>
+     </Routes>   
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
